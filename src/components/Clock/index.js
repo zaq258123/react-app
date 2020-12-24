c0211c1f42cda1b5bf321b2f381bee4e5ee25cb2
Ryan Chan <zaq258123@gmail.com>
2020-12-24 15:56:07 +0800

    chore(structure): restrcture files

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Clock.css';
+import './style.css';
 
 class Clock extends React.Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Clock from './Clock';
+import Clock from './components/Clock';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
